6773b3ce60e20696ea42d6a967ddbba41dee77ee
Daryl <knoxjettadc@gmail.com>
2025-05-01 12:25:33 -0400

    Initial Commit

---
+++ Beyblade X Score 3/Beyblade_X_Score_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Beyblade_X_Score_3App.swift
+//  Beyblade X Score 3
+//
+//  Created by Daryl Chaponot on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Beyblade_X_Score_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beyblade X Score 3Tests/Beyblade_X_Score_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Beyblade_X_Score_3Tests.swift
+//  Beyblade X Score 3Tests
+//
+//  Created by Daryl Chaponot on 5/1/25.
+//
+
+import Testing
+@testable import Beyblade_X_Score_3
+
+struct Beyblade_X_Score_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
